ca96123d46e4dd616d3445e7a1008ff4203c821e
Nick Burnell <nicholas.burnell92@gmail.com>
2018-05-08 23:08:46 -0400

    added media queries, fixed jQuery issues

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 "use strict";
-$(document).click((e) => {
+$(document).ready(() => {
     let tableSelect;
-    if ($(e.target).hasClass('available')) {
-        console.log('ajekf')
-        $('.reserveNow').fadeIn(2000);
-        $('#saveBtn').click(() => {
-            console.log('this worked');
-            $(e.target).addClass("reserved").removeClass("available")
-            console.log('wow this worked?')
-            $('.reserveNow').fadeOut(2000);
-        });
-        $('#exitBtn').click(() => {
-            console.log('that worked');
-            $('.reserveNow').fadeOut(2000);
-        });
+    let name;
+    let size;
+    let table;
+    let info = {}
+    
+
+
+   
+    $('.available').on('click', (event) => {
+        table = $(event.target);
+        $('.reserveNow').fadeIn(1000);
+        console.log('available cont. clicked');
+    }); 
+    
+    $('#exitBtn').on("click", (event) => {
+        console.log(event.target);
+        event = ''; // dunno about this..?
+        $('.reserveNow').fadeOut(1000);
+        console.log('exit btn pressed');
+    });
+
+    $('#saveBtn').on('click', (event) => {
+        name = $('#name').val();
+        size = $('#size').val();
+        $('#name').val('');
+        $('#size').val('');
+        $('#phone').val('');
+        table.attr('name', name);
+        table.attr('size', size);
+        table.addClass('reserved').removeClass('available');
+        addHoverData();
+        $('.reserveNow').fadeOut(1000);
+        console.log(name);
+        console.log(size);
+        
+    });
+
+    function addHoverData() {
+        table.append("<div class=\"hoverData\"><div>Guest Name: " + name + "</div><div>Party Size: " + size + "</div></div>");
     }
+
+    $('.reserved').on('mouseenter', (event) => {
+        info.name = $(event.target).attr('name');
+        info.size = $(event.target).arrt('size');
+        console.log(info.name);
+        console.log(info.size);
+        
+    }).on('mouseleave', (event) => {
+
+    });
+
 });
